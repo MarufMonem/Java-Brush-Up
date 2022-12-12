85ee60f8e75ff792fec8732cf1d43879f672f909
Maruf_Monem <marufmonem@gmail.com>
2022-12-12 10:09:41 +0600

    Do While loop demo

---
+++ src/test/java/doWhileDemo.java
@@ -0,0 +1,11 @@
+public class doWhileDemo {
+    public static void main(String[] args) {
+        int i =0;
+        do {
+//            this is executed first
+            i++;
+            System.out.println(i);
+        }while (i<10);
+//        Then the condition runs
+    }
+}
